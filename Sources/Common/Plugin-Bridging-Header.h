b087b881c22b06def451fd5752f1f51b3f071d47
Jarno Le Conté <jarno.leconte@me.com>
2019-10-21 22:57:31 +0200

    Expose ESDUtilities in Swift

--- Sources/Common/Plugin-Bridging-Header.h
+++ Sources/Common/Plugin-Bridging-Header.h
@@ -2,6 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ESDEventsProtocol.h"
 #import "ESDConnectionManager.h"
 #import "ESDSDKDefines.h"
+#import "ESDEventsProtocol.h"
+#import "ESDUtilities.h"
